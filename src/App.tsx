68b4c03640e152a614bb061c70354f2106aa6e85
Sophia Heicksen <heicksen@udel.edu>
2025-04-01 13:53:44 -0400

    Removed unused logo variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import { NavBar } from './components/navbar';
