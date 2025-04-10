b7d14261d48dee1a878a80f6fec2d90350055593
codeBodger <diesel.ack+github@gmail.com>
2025-04-09 20:00:57 -0400

    TSDocs for login.ts

--- src/functions/login.ts
+++ src/functions/login.ts
@@ -1,8 +1,16 @@
+/**
+ * @function logOut Deletes the name of the user from localStorage and reloads
+ */
 export function logOut() {
     localStorage.removeItem("usrnm");
     window.location.reload();
 }
 
+/**
+ * @function loggedIn Gets the currently logged in user;
+ * can be used to test if there are any logged in users
+ * @returns {string | null} The user, null if none logged in
+ */
 export function loggedIn(): string | null {
     return localStorage.getItem("usrnm");
 }
