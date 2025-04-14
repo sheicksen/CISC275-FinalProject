ad241bc22dcbdf0327e7f4882acedbc7fc8bc22f
Sophia Heicksen <heicksen@udel.edu>
2025-04-14 11:46:18 -0400

    Added answer state to ScaledQuestionTile. Removed submit button.

--- src/components/scaled-question.tsx
+++ src/components/scaled-question.tsx
@@ -1,12 +1,18 @@
-import { Form, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { Question } from "../interfaces/question";
 import "./css/question-tile.css";
+import { useState } from "react";
+
 interface ScaledQuestionProps {
     id:number,
     question:Question<"scaled">
 }
 export function ScaledQuestionTile({id, question}:ScaledQuestionProps){
+    let [answer, setAnswer] = useState(-1);
     let scale = ["1", "2", "3", "4", "5"]
+    let changeAnswer = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(parseInt(e.target.value));
+    }
     return (
         <div className="tile">
             <Form>
@@ -17,15 +23,17 @@ export function ScaledQuestionTile({id, question}:ScaledQuestionProps){
                     {scale.map((num:string)=>(
                         <Form.Check
                         inline
+                        key={num}
                         type="radio"
+                        onChange={changeAnswer}
                         label={num === "1" ? question.scale[0] : num === "5" ? question.scale[1] : num}
                         name={"group-" + id.toString()}
                         id={num}
+                        value={num}
                         ></Form.Check>
                     )
                     )}
                 </div>
-                <Button className="button-color">Submit</Button>
             </Form>
         </div>
     );
