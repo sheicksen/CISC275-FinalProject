ca869fef67c9eb913adf51157f01d38f177933be
Sophia Heicksen <heicksen@udel.edu>
2025-04-14 11:50:12 -0400

    Added answer state to TextQuestionTile. Removed submit button.

--- src/components/text-question.tsx
+++ src/components/text-question.tsx
@@ -1,5 +1,6 @@
 import { Form, Button } from "react-bootstrap";
 import { BasicQuestion } from "../interfaces/question";
+import { useState } from "react";
 import "./css/question-tile.css";
 
 interface TextQuestionProps {
@@ -7,14 +8,17 @@ interface TextQuestionProps {
     question:BasicQuestion
 }
 export function TextQuestionTile({id, question}:TextQuestionProps){
+    let [answer, setAnswer] = useState("");
+    let changeAnswer=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        setAnswer(e.target.value);
+    }
     return (
         <div className="tile">
             <Form>
                 <Form.Text className="text-color">
                     {(id+1).toString() + ". " + question.question}
                 </Form.Text>
-                <Form.Control type="textarea"></Form.Control>
-                <Button className="button-color">Submit</Button>
+                <Form.Control type="textarea" onChange={changeAnswer}></Form.Control>
             </Form>
         </div>
     );
