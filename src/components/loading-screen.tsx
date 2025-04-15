35cfe88e3c81a60cc8818db4ba7ac556f07e895e
codeBodger <diesel.ack+github@gmail.com>
2025-04-14 21:24:42 -0400

    Gittery, but centered loading

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -2,6 +2,7 @@ import "./css/loading-screen.css"
 import { useState } from "react"
 
 const delay = 300;
+const dotrange = {low: 0, high: 3};
 
 interface LoadingScreenProps {
     text: string
@@ -9,13 +10,13 @@ interface LoadingScreenProps {
 export function LoadingScreen({text}: LoadingScreenProps): React.JSX.Element {
     // const [_, set_] = useState<boolean>(false);
     // function reload(): void {set_(!_)}
-    const [dots, setDots] = useState<number>(0);
+    const [dots, setDots] = useState<number>(dotrange.low);
 
-    window.setTimeout(() => {setDots(dots === 3 ? 0 : dots + 1)}, delay);
+    window.setTimeout(() => {setDots(dots >= dotrange.high ? dotrange.low : dots + 1)}, delay);
 
     return (
         <div id="loading-screen">
-            { `${text}${".".repeat(dots)}` }
+            <div id="text"><span id="inv-dots">{".".repeat(dots)}</span>{ `${text}${".".repeat(dots)}` }</div>
         </div>
     )
 }
