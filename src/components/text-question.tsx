9afdd44c29d32999fb59750e530a3258affde7e6
Sophia Heicksen <heicksen@udel.edu>
2025-04-27 22:48:51 -0400

    Created validateText function that checks if a user's text response is not just characters/white space

--- src/components/text-question.tsx
+++ src/components/text-question.tsx
@@ -1,15 +1,36 @@
 import { Form } from "react-bootstrap";
 import { Question } from "../interfaces/question";
 import "./css/question-tile.css";
+import "./css/login.css"
+import { useState } from "react";
 
 interface TextQuestionProps {
     id: number,
     question:Question
     passAnswer: (id:number, question:Question, answer:string)=>void
 }
+/**
+ * @function validateText checks that a string is not all white space and/or punctuation and contains more than a few characters.
+ * @param text that the user entered.
+ * @returns a boolean value representing whether the string is valid or not.
+ */
+function validateText(text:string):boolean{
+    let valid = true;
+    let strippedText = text.replace(/\s/g, "")
+    if(strippedText.length < 3){
+        valid = false;
+    }
+    return valid;
+}
 export function TextQuestionTile({id, question, passAnswer}:TextQuestionProps){
-    let changeAnswer=(e:React.ChangeEvent<HTMLInputElement>)=>{
-        passAnswer(id, question, e.target.value);
+    let [valid, setValid] = useState<boolean>(true);
+    const changeAnswer=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        if (validateText(e.target.value)){
+            setValid(true);
+            passAnswer(id, question, e.target.value);
+        } else {
+            setValid(false);
+        }
     }
     return (
         <div className="tile">
@@ -17,8 +38,13 @@ export function TextQuestionTile({id, question, passAnswer}:TextQuestionProps){
                 <Form.Text className="text-color">
                     {(id+1).toString() + ". " + question.question}
                 </Form.Text>
-                <Form.Control type="textarea" onChange={changeAnswer}></Form.Control>
+                <Form.Control required type="textarea" onChange={changeAnswer}></Form.Control>
             </Form>
+            {!valid &&
+                <div id="login-failure" className="align-to-button">
+                    {`For the best experience, ensure your response is valid.`}
+                </div>
+            }
         </div>
     );
 }
