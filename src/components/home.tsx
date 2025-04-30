263c322b34517519610d7a129656aa8099c904a8
Henry Leap <henrywl@udel.edu>
2025-04-30 12:13:03 -0400

    Updated quiz descriptions

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -18,15 +18,15 @@ export function Home({selectPage}: HomeProps): React.JSX.Element {
                 </Col>
                 <Col>
                     <h1> Seeking Answers?</h1>
-                    <p>Not sure where you see yourself in the future? You're only one click away from finding out.</p>
+                    <p>Knowing what career you want to pursue is difficult. Narrowing down the thousands of possible ways to make money into a single profession, or even a field of study can take lots of research online, discussions with experienced workers, and introspection into your own preferences. While we can’t do all that work for you, our quiz can help you understand what you want from your career, as well as what some careers look like, should you choose to pursue them.</p>
                     <Button className="button-style basic-quiz-button" style={{fontSize: "45px", borderWidth: "0.6vh"}} onClick={()=>{
                         selectPage("Basic Questions");
                     }}>Start Quiz</Button><br/>
                     <div>
-                        <p style={{fontSize: "24px", display:"inline"}}>Have some extra time to spend? Try out our &#8203;
+                        <p style={{fontSize: "24px", display:"inline"}}>Already know what area you want to work in? Looking for more than just a broad-strokes assessment? Talk to an AI in our &#8203;
                         <button className="link-button" onClick={()=>{
                             selectPage("Detailed Questions");
-                        }}>Detailed Quiz</button>.</p>
+                        }}>Detailed Quiz</button> to learn about the specifics of a career field.</p>
                     </div>
                 </Col>
             </Row>
