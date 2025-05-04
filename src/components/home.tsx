254b423807f77104ba093f247863fa7796af12c3
Sophia Heicksen <heicksen@udel.edu>
2025-05-04 19:45:15 -0400

    Added background stripes

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -16,7 +16,7 @@ export function Home({selectPage}: HomeProps): React.JSX.Element {
                         <img src={Image} alt="A sillouette of a man with a white question mark on his chest"></img>
                     </div>
                 </Col>
-                <Col>
+                <Col className="desc-container">
                     <h1> Seeking Answers?</h1>
                     <p style={{fontSize: "22px", display:"inline"}}>Knowing what career you want to pursue is difficult. Narrowing down the thousands of possible ways to make money into a single profession, or even a field of study can take lots of research online, discussions with experienced workers, and introspection into your own preferences. While we can’t do all that work for you, our quiz can help you understand what you want from your career, as well as what some careers look like, should you choose to pursue them.</p>
                     <Button className="button-style basic-quiz-button" style={{fontSize: "45px", borderWidth: "0.6vh"}} onClick={()=>{
