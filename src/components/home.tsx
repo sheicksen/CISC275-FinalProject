684bbfd6e2c50115e852503c1bee9921796a8ee8
Sophia Heicksen <heicksen@udel.edu>
2025-05-04 19:55:30 -0400

    Adjusted stripe size and image padding

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -18,12 +18,12 @@ export function Home({selectPage}: HomeProps): React.JSX.Element {
                 </Col>
                 <Col className="desc-container">
                     <h1> Seeking Answers?</h1>
-                    <p style={{fontSize: "22px", display:"inline"}}>Knowing what career you want to pursue is difficult. Narrowing down the thousands of possible ways to make money into a single profession, or even a field of study can take lots of research online, discussions with experienced workers, and introspection into your own preferences. While we can’t do all that work for you, our quiz can help you understand what you want from your career, as well as what some careers look like, should you choose to pursue them.</p>
+                    <p style={{fontSize: "20px", display:"inline"}}>Knowing what career you want to pursue is difficult. Narrowing down the thousands of possible ways to make money into a single profession, or even a field of study can take lots of research online, discussions with experienced workers, and introspection into your own preferences. While we can’t do all that work for you, our quiz can help you understand what you want from your career, as well as what some careers look like, should you choose to pursue them.</p>
                     <Button className="button-style basic-quiz-button" style={{fontSize: "45px", borderWidth: "0.6vh"}} onClick={()=>{
                         selectPage("Basic Questions");
                     }}>Start Quiz</Button><br/>
                     <div>
-                        <p style={{fontSize: "22px", display:"inline"}}>Already know what area you want to work in? Looking for more than just a broad-strokes assessment? Talk to an AI in our &#8203;
+                        <p style={{fontSize: "20px", display:"inline"}}>Already know what area you want to work in? Looking for more than just a broad-strokes assessment? Talk to an AI in our &#8203;
                         <button className="link-button" onClick={()=>{
                             selectPage("Detailed Questions");
                         }}>Detailed Quiz</button>.</p>
