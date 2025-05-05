b971e173ed139ee1bf2adf512d3ea6b8a5470ab5
Sophia Heicksen <heicksen@udel.edu>
2025-05-05 11:32:38 -0400

    Progress bar text now updates. Removed unused css class.

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -9,7 +9,7 @@ export function ProgBar({totalQuestions, answeredQuestions}:ProgressBarProps){
     let percent=Math.ceil((answeredQuestions/totalQuestions)*100);
     return (
         <div className={percent >= 99 ? "sticky-bar complete-quiz" : "sticky-bar in-progress"}>
-            <ProgressBar animated now={percent} label="Quiz Progress"></ProgressBar>
+            <ProgressBar animated now={percent} label={percent >= 99 ? "Quiz Complete!":"Quiz Progress"}></ProgressBar>
         </div> 
     );
 }
