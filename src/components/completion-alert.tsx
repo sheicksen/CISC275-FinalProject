de70c9e92935c758423a1065fac86060ec03c43f
Sophia Heicksen <heicksen@udel.edu>
2025-05-11 17:31:31 -0400

    Adjusted margins and centering of various components

--- src/components/completion-alert.tsx
+++ src/components/completion-alert.tsx
@@ -6,7 +6,9 @@ import "./css/completion-alert.css";
 export function CompletionAlert(/* {enabled}: CompletionAlertProps */) {
     return (
         <div id="completion-alert">
-            You've completed all of the questions.  Click below for results!
+            You've completed all of the questions.
+            <br></br>
+            Click below for results!
         </div> 
     );
 }
