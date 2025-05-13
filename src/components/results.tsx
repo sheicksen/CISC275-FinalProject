c990a957abe5f936f0a0cd5c1efaeec60f898061
codeBodger <diesel.ack+github@gmail.com>
2025-05-13 00:40:47 -0400

    ToDo

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -50,6 +50,11 @@ export function Results({setLoading, promisedAnalysis, setQuizRunName, setAppAna
     )) 
     : <p>Here, you'll see your results from previous quizzes</p>;
 
+    /* ToDo:
+        - Make them actually save
+        - Make it not look horrid
+    */
+
     function updateResSetName(event: React.ChangeEvent<HTMLInputElement>) {
         setResSetName(event.target.value);
     }
